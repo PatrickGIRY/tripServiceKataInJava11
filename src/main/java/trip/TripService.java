a81c17e9f3cd00a99602b059ec4fc58de5b2035a
Patrick GIRY <patrickgiry@gmail.com>
2018-09-25 16:16:32 +0200

    Move `isFriend` into the `if` block.

--- src/main/java/trip/TripService.java
+++ src/main/java/trip/TripService.java
@@ -12,8 +12,8 @@ public class TripService {
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
 		if (loggedUser != null) {
+			boolean isFriend = false;
 			for (User friend : user.getFriends()) {
 				if (friend.equals(loggedUser)) {
 					isFriend = true;
