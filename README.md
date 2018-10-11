b5c4a4f1663d22525d34a5c7857c658355f9dbef
Patrick GIRY <patrickgiry@gmail.com>
2018-10-11 11:45:49 +0200

    Migrate to JDK 11.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Trip service kata in Java 10
+# Trip service kata in Java 11
 
 Made from [Sandro Mancuso trip service kata](https://github.com/sandromancuso/trip-service-kata.git)
--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 with import <nixpkgs> {};
 let 
-mvn = maven.override { jdk="/Library/Java/JavaVirtualMachines/jdk-10.jdk/Contents/Home/"; };
+mvn = maven.override { jdk="/Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home"; };
 in
 stdenv.mkDerivation {
-   name = "mvn-java10";
-   JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-10.jdk/Contents/Home/";
+   name = "mvn-java11";
+   JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-11.jdk/Contents/Home";
    buildInputs = [mvn];
 }
 
