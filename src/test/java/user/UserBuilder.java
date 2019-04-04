b6216644e56cf0c1dfc06dac9502ef1fdd8e68fb
Patrick GIRY <patrickgiry@gmail.com>
2019-04-04 10:44:37 +0200

    Refactor `UserBuilder`.

--- src/test/java/user/UserBuilder.java
+++ src/test/java/user/UserBuilder.java
@@ -2,41 +2,30 @@ package user;
 
 import trip.Trip;
 
-import java.lang.module.ModuleDescriptor;
 import java.util.stream.Stream;
 
 public class UserBuilder {
-    private User[] friends = {};
-    private Trip[] trips = {};
+    private User user;
 
     public static UserBuilder aUser() {
         return new UserBuilder();
     }
 
-    private UserBuilder() {}
+    private UserBuilder() {
+        user = new User();
+    }
 
     public UserBuilder friendsWith(User ...friends) {
-        this.friends = friends;
+        Stream.of(friends).forEach(user::addFriend);
         return this;
     }
 
     public UserBuilder withTrips(Trip ...trips) {
-        this.trips = trips;
+        Stream.of(trips).forEach(user::addTrip);
         return this;
     }
 
     public User build() {
-        User user = new User();
-        addFriendsTo(user);
-        addTripsTo(user);
         return user;
-    }
-
-    private void addTripsTo(User user) {
-        Stream.of(trips).forEach(user::addTrip);
-    }
-
-    private void addFriendsTo(User user) {
-        Stream.of(friends).forEach(user::addFriend);
     }
 }
