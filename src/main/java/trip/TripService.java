31687a043355b493e516aec504215e1226a5d69b
Abderrahmen KALLEL <abderrahmen.kallel-extern@renault.com>
2019-04-04 15:58:19 +0200

    test return_trip_list_when_user_is_a_friend_of_logged_user

--- src/main/java/trip/TripService.java
+++ src/main/java/trip/TripService.java
@@ -21,7 +21,7 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
+				tripList = findTripsBy(user);
 			}
 			return tripList;
 		} else {
@@ -29,7 +29,11 @@ public class TripService {
 		}
 	}
 
-    User loggedUser() {
+	List<Trip> findTripsBy(User user) {
+		return TripDAO.findTripsByUser(user);
+	}
+
+	User loggedUser() {
         return UserSession.getInstance().getLoggedUser();
     }
 
--- src/test/java/trip/TripServiceShould.java
+++ src/test/java/trip/TripServiceShould.java
@@ -4,6 +4,8 @@ import exception.UserNotLoggedInException;
 import org.junit.jupiter.api.Test;
 import user.User;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -50,6 +52,15 @@ class TripServiceShould {
 
     }
 
+    @Test
+    void return_trip_list_when_user_is_a_friend_of_logged_user(){
+        TripService tripService= new TestableTripService(REGISTRED_USER);
+        User friendUser = new User();
+        friendUser.addFriend(REGISTRED_USER);
+        Trip trip = new Trip();
+        friendUser.addTrip(trip );
+        assertThat(tripService.getTripsByUser(friendUser)).contains(trip);
+    }
 
     class TestableTripService extends TripService {
 
@@ -62,6 +73,11 @@ class TripServiceShould {
         @Override
         User loggedUser() {
             return loggedUser;
+        }
+
+        @Override
+        List<Trip> findTripsBy(User user) {
+            return user.trips();
         }
     }
 }
