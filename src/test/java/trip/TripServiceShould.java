91e223643e3216995cdfd1f21ccd722c1cb6ca59
obouallou <omar.bouallou@renault.com>
2019-04-04 15:24:00 +0200

    add test for users with no friends

--- src/test/java/trip/TripServiceShould.java
+++ src/test/java/trip/TripServiceShould.java
@@ -4,6 +4,7 @@ import exception.UserNotLoggedInException;
 import org.junit.jupiter.api.Test;
 import user.User;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TripServiceShould {
@@ -11,12 +12,13 @@ class TripServiceShould {
 
     private static final User UNUSED_USER = null;
     private static final User GUEST = null;
+    private static final User REGISTRED_USER = new User();
 
 
     @Test
     void throw_exception_if_user_not_logged_in() {
 
-        TripService tripService = new TestableTripService();
+        TripService tripService = new TestableTripService(GUEST);
 
         assertThatThrownBy(() ->
                 tripService.getTripsByUser(UNUSED_USER))
@@ -24,11 +26,28 @@ class TripServiceShould {
 
     }
 
+
+    @Test
+    void return_empty_trip_list_when_user_has_no_friends()
+    {
+        TripService tripService = new TestableTripService(REGISTRED_USER);
+
+        User noFriendUser = new User();
+        assertThat(tripService.getTripsByUser(noFriendUser)).isEmpty();
+    }
+
+
     class TestableTripService extends TripService {
+
+        private User loggedUser;
+
+
+        TestableTripService(User loggedUser) {this.loggedUser = loggedUser;}
+
 
         @Override
         User loggedUser() {
-            return GUEST;
+            return loggedUser;
         }
     }
 }
